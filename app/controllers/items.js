4b84083730daf06b4647c856452014e0ecc06c66
DSRoden <Daniel.S.Roden@gmail.com>
2014-08-30 19:10:07 -0500

    Fixed semicolon error

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -25,5 +25,5 @@ exports.itemBidPage= function(req, res){
   Item.findTradeAndBiddableItems(req.params.itemId, res.locals.user._id, function(err,item, biddableItems){
     res.render('items/show-bid', {item:item, biddableItems:biddableItems});
   });
-}
+};
 
