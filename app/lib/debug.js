97540353303f2cdaa636935c2a2c63eff6213fdc
Adam Barnhard <admbarn@gmail.com>
2014-09-01 13:56:50 -0500

    half done with debugger

--- app/lib/debug.js
+++ app/lib/debug.js
@@ -6,27 +6,40 @@ exports.info = function(req, res, next){
   var debug = process.env.DEBUG * 1;
 
   if(debug){
-    console.log(chalk.bgRed('--------------------------------------------------------------------------------'));
-    console.log(chalk.bold.red('TIME   :'), chalk.bold.blue(new Date()));
-    console.log(chalk.bold.red('PORT   :'), process.env.PORT);
-    console.log(chalk.bold.red('DB     :'), process.env.DB);
-    console.log(chalk.bold.red('URL    :'), req.url);
-    console.log(chalk.bold.red('VERB   :'), req.method);
-    console.log(chalk.bold.green('PARAMS :'), req.params);
-    console.log(chalk.bold.green('QUERY  :'), req.query);
-    console.log(chalk.bold.green('BODY   :'), req.body);
-    console.log(chalk.bold.green('LOCALS :'), res.locals);
-    console.log(chalk.bold.green('SESSION:'), req.session);
-    console.log(chalk.bold.red('SESSID :'), req.sessionID);
+    console.log(debug ? chalk.bgRed('--------------------------------------------------------------------------------') : '');
+    console.log(debug ? chalk.bold.red('TIME:') : '');
+    console.log(debug ? chalk.bold.blue(new Date()) : '');
+    console.log(debug ? chalk.bold.red('PORT:') : '');
+    console.log(debug ? process.env.PORT : '');
+    console.log(debug ? chalk.bold.red('DB:') : '');
+    console.log(debug ? process.env.DB : '');
+    console.log(debug ? chalk.bold.red('URL:') : '');
+    console.log(debug ? req.url : '');
+    console.log(debug ? chalk.bold.red('VERB:') : '');
+    console.log(debug ? req.method : '');
+    console.log(debug ? chalk.bold.green('PARAMS:') : '');
+    console.log(debug ? req.params : '');
+    console.log(debug ? chalk.bold.green('QUERY:') : '');
+    console.log(debug ? req.query : '');
+    console.log(debug ? chalk.bold.green('BODY:') : '');
+    console.log(debug ? req.body : '');
+    console.log(debug ? chalk.bold.green('LOCALS:') : '');
+    console.log(debug ? res.locals : '');
+    console.log(debug ? chalk.bold.green('SESSION:') : '');
+    console.log(debug ? req.session : '');
+    console.log(debug ? chalk.bold.red('SESSID:') : '');
+    console.log(debug ? req.sessionID : '');
 
     if(debug === 2){
-      console.log(chalk.bgBlue('--------------------------------------------------------------------------------'));
-      console.log(chalk.bold.red('HEADERS:'), req.headers);
-      console.log(chalk.bgBlue('--------------------------------------------------------------------------------'));
-      console.log(chalk.bold.red('ENV    :'), process.env);
+      console.log(debug === 2 ? chalk.bgBlue('--------------------------------------------------------------------------------') : '');
+      console.log(debug === 2 ? chalk.bold.red('HEADERS:') : '');
+      console.log(debug === 2 ? req.headers : '');
+      console.log(debug === 2 ? chalk.bgBlue('--------------------------------------------------------------------------------') : '');
+      console.log(debug === 2 ? chalk.bold.red('ENV:') : '');
+      console.log(debug === 2 ? process.env : '');
     }
 
-    console.log(chalk.bgRed('--------------------------------------------------------------------------------'));
+    console.log(debug ? chalk.bgRed('--------------------------------------------------------------------------------') : '');
   }
 
   next();
